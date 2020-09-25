4fc6e8135894c730c1a7a188b95a4523328661b8
Edgar Diaz-Gutierrez <ediaz-gutierrez@hotmail.com>
2020-09-24 23:51:40 -0700

    football is learning exercise teaching database creation

---
+++ 04-java-spring/01-spring-fundamentals/football/src/main/java/com/codingdojo/football/repositories/TeamRepository.java
@@ -0,0 +1,23 @@
+package com.codingdojo.football.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codingdojo.football.models.Team;
+
+@Repository
+public interface TeamRepository extends CrudRepository<Team, Long> {
+	List<Team> findAll();
+	
+	
+}
+
+
+/*
+C - Create
+R - Read
+U - Update
+D - Delete
+*/
