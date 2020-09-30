7792275b0fbbdc3d5be873e520983fb9859a5a2a
Edgar Diaz-Gutierrez <ediaz-gutierrez@hotmail.com>
2020-09-30 08:10:51 -0700

    created database and empty jsp files for Lookify assignment

---
+++ 04-java-spring/02-spring-data-i/04-lookify/src/main/java/com/edgar/lookify/controllers/SongController.java
@@ -0,0 +1,14 @@
+package com.edgar.lookify.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.edgar.lookify.services.SongService;
+
+@Controller
+public class SongController {
+	@Autowired
+	private SongService sService;
+	
+	//Routes
+}
